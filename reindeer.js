1f3bb8ccd9ac3852216dedb24f7f413e05255ba3
Haden Hudson <mccoy271@gmail.com>
2018-01-26 15:54:26 -0600

    final reindeer

---
+++ reindeer.js
@@ -0,0 +1,11 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (let i = 0; i < reindeer.length; i += 1){
+    var reindeerColor = colors[i] + " " + reindeer[i] + "<br>";
+
+    hohohoElement.innerHTML += reindeerColor;
+}
